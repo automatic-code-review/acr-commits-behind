988edba07924c89a19c8344eca3ff434130988b9
kielsonzinn <kielsonzinn@gmail.com>
2024-08-09 16:46:33 -0300

    feat: Implementa extensão

--- src/review.py
+++ src/review.py
@@ -1,25 +1,45 @@
+from http import HTTPStatus
+
 import automatic_code_review_commons as commons
+import requests
+
 
 def review(config):
-    path_target = config['path_target']
-    path_source = config['path_source']
+    git_url = config['git_url']
+    git_token = config['git_token']
 
     merge = config['merge']
     project_id = merge['project_id']
-    merge_request_id = merge['merge_request_id']
-    
+    from_project_id = merge['from_project_id']
+    branch_target = merge['branch']['target']
+    branch_source = merge['branch']['source']
+
+    url = (f"{git_url}/api/v4/projects/{project_id}/repository/compare?"
+           f"from={branch_source}&to={branch_target}&from_project_id={from_project_id}")
+
+    response = requests.get(url, headers={
+        'Private-Token': git_token
+    })
+
     comments = []
-    
-    # TODO IMPLEMENTAR EXTENSION
-    #  O OBJETO DE COMENTARIO DEVE POSSUIR O SEGUINTE FORMATO
-    #  commons.comment_create(
-    #     comment_id=commons.comment_generate_id( "" ),
-    #     comment_path="",
-    #     comment_description="",
-    #     comment_snipset=True,
-    #     comment_end_line=1,
-    #     comment_start_line=1,
-    #     comment_language="",
-    # )
+    status_code = {response.status_code}
+
+    if status_code == HTTPStatus.OK:
+        data = response.json()
+        commits_behind = len(data["commits"])
+        commits_behind_limit = config["commitsBehindLimit"]
+
+        if commits_behind > commits_behind_limit:
+            descr_message = config['message']
+            descr_message = descr_message.replace("${COMMITS_BEHIND}", str(commits_behind))
+            descr_message = descr_message.replace("${COMMITS_BEHIND_LIMIT}", str(commits_behind_limit))
+
+            comments.append(commons.comment_create(
+                comment_id=commons.comment_generate_id(descr_message),
+                comment_description=descr_message,
+                comment_path=None
+            ))
+    else:
+        raise OSError(f"Não foi possivel buscar os commits behind. Status code {status_code}")
 
     return comments
