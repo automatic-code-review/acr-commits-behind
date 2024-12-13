3055e0c91b1323bd4e2e53ec731247ac44cb98e6
kielsonzinn <kielsonzinn@gmail.com>
2024-12-13 08:48:19 -0300

    feat: Permitir configurar o resolver policy

--- src/review.py
+++ src/review.py
@@ -13,11 +13,16 @@ def review(config):
         descr_message = descr_message.replace("${COMMITS_BEHIND}", str(commits_behind_current))
         descr_message = descr_message.replace("${COMMITS_BEHIND_LIMIT}", str(commits_behind_limit))
 
-        comments.append(commons.comment_create(
+        comment = commons.comment_create(
             comment_id=commons.comment_generate_id(descr_message),
             comment_description=descr_message,
             comment_path=None,
             comment_snipset=False,
-        ))
+        )
+
+        if 'processorArgs' in config:
+            comment['processorArgs'] = config['processorArgs']
+
+        comments.append(comment)
 
     return comments
